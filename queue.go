254ce9c4f996e15be2c7b5b3a2e0d122db178978
Brett Dutton <brett@b2bconsultancy.asia>
2021-09-24 21:40:49 +0700

    Updated to have close for queue

--- queue.go
+++ queue.go
@@ -5,6 +5,7 @@ import "fmt"
 type Queue struct {
 	c        chan interface{}
 	capacity int
+	closed   bool
 }
 
 // NewQueue name says it all
@@ -17,6 +18,10 @@ func NewQueue(capacity int) *Queue {
 
 // Put a value in the queue
 func (q *Queue) Put(val interface{}) error {
+	if q.closed {
+		return fmt.Errorf("queue is closed")
+	}
+
 	select {
 	case q.c <- val:
 		return nil
@@ -27,10 +32,22 @@ func (q *Queue) Put(val interface{}) error {
 
 // Gets a value from the queue return nil if the queue is empty
 func (q *Queue) Get() interface{} {
+	if q.closed {
+		return nil
+	}
+
 	select {
 	case val := <-q.c:
 		return val
 	default:
 		return nil
 	}
+}
+
+func (q *Queue) Close() {
+	if q.closed {
+		return
+	}
+	q.closed = true
+	close(q.c)
 }
